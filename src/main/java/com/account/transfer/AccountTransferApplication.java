1dee96b9af1b87e808c4ad8ebd595121e9890bab
Golomoz.Vadym <v.golomoz@andersenlab.com>
2024-06-07 13:01:14 +0300

    added dependencies and project structure

---
+++ src/main/java/com/account/transfer/AccountTransferApplication.java
@@ -0,0 +1,13 @@
+package com.account.transfer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountTransferApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountTransferApplication.class, args);
+	}
+
+}
