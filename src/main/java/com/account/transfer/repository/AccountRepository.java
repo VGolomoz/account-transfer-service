65bdad7814999425a9f57f9e7229fe8ea37b92aa
Golomoz.Vadym <v.golomoz@andersenlab.com>
2024-06-08 15:34:19 +0300

    added repositories

---
+++ src/main/java/com/account/transfer/repository/AccountRepository.java
@@ -0,0 +1,9 @@
+package com.account.transfer.repository;
+
+import com.account.transfer.entity.AccountEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AccountRepository extends JpaRepository<AccountEntity, Long> {
+}
