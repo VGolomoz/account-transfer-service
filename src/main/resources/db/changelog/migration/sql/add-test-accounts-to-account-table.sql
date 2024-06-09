4d100c71b0447610d63bc01cfb88ca31c417b4c7
Golomoz.Vadym <v.golomoz@andersenlab.com>
2024-06-09 19:29:12 +0300

    added liquibase scripts for test data

---
+++ src/main/resources/db/changelog/migration/sql/add-test-accounts-to-account-table.sql
@@ -0,0 +1,14 @@
+INSERT INTO account (balance, currency, owner_id)
+VALUES (1500.50, 'USD', 1001);
+
+INSERT INTO account (balance, currency, owner_id)
+VALUES (2500.75, 'EUR', 1002);
+
+INSERT INTO account (balance, currency, owner_id)
+VALUES (3000.00, 'USD', 1003);
+
+INSERT INTO account (balance, currency, owner_id)
+VALUES (4500.25, 'JPY', 1004);
+
+INSERT INTO account (balance, currency, owner_id)
+VALUES (5000.50, 'GBP', 1005);
